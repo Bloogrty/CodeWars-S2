48005986b4267fc891dac3ac0026bb509f914a11
Cristanto <cristanto.sidharta@gmail.com>
2025-03-23 10:13:38 +0700

    case 34

---
+++ case-34.js
@@ -0,0 +1,8 @@
+// Name:Convert a String to a Number!
+// Link:https://www.codewars.com/kata/544675c6f971f7399a000e79/train/javascript
+// Solution:
+
+const stringToNumber = function (str) {
+  // put your code here
+  return Number(str);
+};
